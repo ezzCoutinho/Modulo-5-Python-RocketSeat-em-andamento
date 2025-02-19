cc89587229269545bcd2dbe3f55062d21c93ab98
ezzcoutinho <lammerlopes@hotmail.com>
2025-02-18 22:49:31 -0300

    Configure Flask app with database settings and initialize SQLAlchemy

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, jsonify
+from repository.database import db
+import os 
+from db_models.payment import Payment
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "database.db")}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Evita warnings
+app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
+
+db.init_app(app)
 
 @app.route('/payments/pix', methods=['POST'])
 def create_payment_pix():
