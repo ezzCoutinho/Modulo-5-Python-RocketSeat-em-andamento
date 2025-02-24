283a295fe236d82a9f8e58994211ca00e06ae6c9
ezzcoutinho <lammerlopes@hotmail.com>
2025-02-24 20:22:34 -0300

    Integrate Flask-SocketIO for real-time payment updates and handle client connections

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from repository.database import db
 from db_models.payment import Payment
 from datetime import datetime, timedelta
 from payments.pix import Pix
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
 
 db.init_app(app)
+socketio = SocketIO(app)
 
 @app.route('/payments/pix', methods=['POST'])
 def create_payment_pix():
@@ -44,5 +46,9 @@ def payment_pix_page(payment_id):
   payment = Payment.query.get(payment_id)
   return render_template("payment.html", payment_id=payment.id, value=payment.value, qr_code=payment.qr_code, expiration_date=payment.expiration_date, host="http://127.0.0.1:5000")
 
+@socketio.on('connect')
+def handle_connect():
+  print('Client connected to the server')
+
 if __name__ == '__main__':
-  app.run(debug=True)
+  socketio.run(app, debug=True)
